79b8371564533b2a82424de0b81a0ae54060fdec
MO <matty.intoxicado@gmail.com>
2020-09-19 20:40:06 -0300

    detectar so

--- src/experimentacion/general.py
+++ src/experimentacion/general.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from matplotlib import rc, rcParams, axes
 from matplotlib import axes as mplibAxes
 import pandas as pd
+import platform
 
 # class partido:
 #     def __init__(self, fecha, iEquipo, iGoles, jEquipo, jGoles):
@@ -17,7 +18,11 @@ import pandas as pd
 #         self.jEquipo = jEquipo
 
 def tprun(algoritmo, input, output):
-    comando = ["../tp", input, output, algoritmo]
+    ejecutable = "../tp"
+    system = platform.system()
+    if system == "Windows":
+        ejecutable = "../tp.exe"
+    comando = [ejecutable, input, output, algoritmo]
     sp.check_call(comando, encoding="utf8")
     
     rankings = []
